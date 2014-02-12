59f92859ffa9be7562945e7a531adb7e46861871
Shiv Shankar <shiv@pobox.com>
2014-02-13 10:50:59 +1100

    change colors when in screen session

--- .zsh/prompt.zsh
+++ .zsh/prompt.zsh
@@ -1,5 +1,8 @@
 autoload colors; colors;
 setopt prompt_subst
 
-PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n) %{$fg_bold[blue]%}%(!.%1~.%~) %_%{$reset_color%} '
-
+if [ -z $STY ]; then
+  PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n) %{$fg_bold[blue]%}%(!.%1~.%~) %_%{$reset_color%} '
+else 
+  PROMPT='%(!.%{$fg_bold[red]%}.%{$fg_bold[green]%}%n) %{$fg_bold[red]%}%(!.%1~.%~) %_%{$reset_color%} '
+fi
