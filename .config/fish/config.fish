b18a59ba444bd9075371a792df3b906660a5e935
Shiv Shankar <shiv.shankar@gtnexus.com>
2019-08-02 16:09:20 -0400

    [fish] more in fish path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,3 @@
 set -x GNU_PATH /usr/local/opt/coreutils/libexec/gnubin /usr/local/opt/gnu-tar/libexec/gnubin /usr/local/opt/gnu-sed/libexec/gnubin /usr/local/opt/grep/libexec/gnubin
+set -x PYTHON_PATH ~/Library/Python/3.7/bin
+set -gx PATH $GNU_PATH $PYTHON_PATH $PATH
